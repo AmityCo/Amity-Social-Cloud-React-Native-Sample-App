a006bc73368bf104d2cc0dd824a6b8c15b317835
Reza Babaei <reza.tizro@gmail.com>
2021-07-23 12:29:52 +0300

    feat: init community part

--- screens/tab/TabList.js
+++ screens/tab/TabList.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Header } from 'react-native-elements';
+import React from "react";
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Header } from "react-native-elements";
 
-import FeedScreen from './Feed';
+import FeedScreen from "./Feed";
+import CommunitiesScreen from "./Communities";
 
 const Tab = createBottomTabNavigator();
 
 function ChatScreen() {
-    return (
-      <View style={{ flex: 1}}>
-          <Header
-          backgroundColor="#26cb7c"
-          centerComponent={{ text: 'Chat', style: { color: '#fff' } }}
-          />
-          <View style={{ flex:1 , alignItems: 'center' ,justifyContent: 'center'}}>
-            <Text>Chat!</Text>
-          </View>
+  return (
+    <View style={{ flex: 1 }}>
+      <Header
+        backgroundColor="#26cb7c"
+        centerComponent={{ text: "Chat", style: { color: "#fff" } }}
+      />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Chat!</Text>
       </View>
-    );
-  }
-
+    </View>
+  );
+}
 
 function TabList() {
   return (
-    <Tab.Navigator >
-      <Tab.Screen name="Chat" component={ChatScreen}/>
+    <Tab.Navigator>
+      <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Settings" component={FeedScreen} />
+      <Tab.Screen name="Communities" component={CommunitiesScreen} />
     </Tab.Navigator>
   );
 }
