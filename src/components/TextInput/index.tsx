498759231f58f29cc68150f2ee41f6aee2038e9d
Reza Babaei <reza.tizro@gmail.com>
2021-07-29 22:01:59 +0300

    feat: add TextInput Component

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { VFC } from 'react';
+import { View, StyleProp, ViewStyle } from 'react-native';
+import { Text, TextInput as TextInputComponent } from 'react-native-paper';
+
+type TextInputProps = React.ComponentProps<typeof TextInputComponent> & {
+  error?: boolean;
+  errorText?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+};
+
+const TextInput: VFC<TextInputProps> = ({
+  error,
+  errorText,
+  dense = true,
+  containerStyle,
+  mode = 'outlined',
+  ...props
+}) => {
+  return (
+    <View style={containerStyle}>
+      <TextInputComponent mode={mode} dense={dense} error={error} {...props} />
+      {error && errorText !== '' && <Text>{errorText}</Text>}
+    </View>
+  );
+};
+
+export default TextInput;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
+export { default as TextInput } from './TextInput';
 export { default as TabBarIcon } from './TabBarIcon';
 export { default as DrawerContent } from './DrawerContent';
