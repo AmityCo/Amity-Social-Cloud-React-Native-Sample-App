4a952d4577a411a53933a14bf395413fcca253a1
Reza Babaei <reza.tizro@gmail.com>
2021-08-25 21:12:18 +0300

    feat: add FormData type to index.d.ts (WIP)

---
+++ FormData.ts
@@ -0,0 +1,19 @@
+declare class FormData {
+  append(name: string, value: string | Blob, fileName?: string): void;
+
+  delete(name: string): void;
+
+  get(name: string): string | null;
+
+  getAll(name: string): string[];
+
+  has(name: string): boolean;
+
+  set(name: string, value: string | Blob, fileName?: string): void;
+
+  forEach(callbackfn: (value: string, key: string, parent: FormData) => void, thisArg?: any): void;
+
+  getParts(name: string): string[];
+}
+
+export default FormData;
