c9d39fbb487ad95f15476e2ede54ad7ecfe08972
Reza Babaei <reza.tizro@gmail.com>
2021-08-25 21:13:09 +0300

    feat: add errror & confirmation alerts

---
+++ src/utils/alerts.ts
@@ -0,0 +1,41 @@
+import { AxiosError } from 'axios';
+import { Alert } from 'react-native';
+
+import { t } from 'i18n';
+
+import getErrorMessage from './getErrorMessage';
+
+export const alertError = (error: Error | AxiosError, onClose?: () => void): void => {
+  Alert.alert(
+    'Oooops!',
+    getErrorMessage(error),
+    [
+      {
+        text: t('close'),
+        onPress: async () => {
+          if (onClose) onClose();
+        },
+      },
+    ],
+    { cancelable: false },
+  );
+};
+
+export const alertConfirmation = (onPress: () => void): void => {
+  Alert.alert(
+    t('are_you_sure'),
+    '',
+    [
+      { text: t('cancel'), style: 'cancel' },
+      {
+        text: t('ok'),
+        onPress: async () => {
+          if (onPress) onPress();
+        },
+      },
+    ],
+    { cancelable: false },
+  );
+};
+
+export default {};
--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,27 +1,7 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Alert } from 'react-native';
 import { AxiosError } from 'axios';
-
-import { t } from 'i18n';
 
 const getErrorMessage = (error: Error | AxiosError | undefined): string => {
   return error?.message ?? 'Error while handling request!';
-};
-
-export const alertError = (error: Error | AxiosError, onClose: () => void): void => {
-  Alert.alert(
-    'Oooops!',
-    getErrorMessage(error),
-    [
-      {
-        text: t('close'),
-        onPress: async () => {
-          onClose();
-        },
-      },
-    ],
-    { cancelable: false },
-  );
 };
 
 export default getErrorMessage;
