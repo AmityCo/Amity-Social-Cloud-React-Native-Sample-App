703e5fbc96d928106fe96e5fcb17aa1c7f35234f
Reza Babaei <reza.tizro@gmail.com>
2021-08-25 21:16:33 +0300

    chore: clear confusing types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,8 @@
 export * from './posts';
-export * from './users';
 export * from './flags';
 export * from './routes';
-export * from './comments';
 export * from './reactions';
 export * from './providers';
-export * from './community';
 
 export enum LoadingState {
   LOADING = 'loading',
--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,40 +1,9 @@
-/* eslint-disable import/prefer-default-export */
-export enum FeedType {
+export enum FeedTargetType {
   Normal = 'normal',
   Global = 'global',
-}
-
-export enum PostSortBy {
-  FIRST_CREATED = 'firstCreated',
-  LAST_CREATED = 'lastCreated',
-  LAST_UPDATED = 'lastUpdated',
-  FIRST_UPDATED = 'firstUpdated',
 }
 
 export enum PostFeedType {
   REVIEWING = 'reviewing',
   PUBLISHED = 'published',
 }
-
-export type AddFeedType = {
-  visible: boolean;
-  isEditId: string;
-  onClose: () => void;
-  targetType: Amity.PostTargetType;
-  targetId: string;
-};
-
-export type AddPostDataType = {
-  data: {
-    text: string;
-    files?: string[];
-    images?: string[];
-  };
-  targetType: string;
-  targetId: string;
-};
-
-export type PostItemProps = {
-  onPress?: () => void;
-  onEditPost?: (postId: string) => void;
-};
