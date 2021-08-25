0272c5256f4138852a57472a33fa09ebdfc33a02
Reza Babaei <reza.tizro@gmail.com>
2021-08-25 23:01:23 +0300

    feat: improve Communities / styles

--- src/screens/Communities/index.tsx
+++ src/screens/Communities/index.tsx
@@ -1,36 +1,35 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import { queryCommunities, createQuery, runQuery } from '@amityco/ts-sdk';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
 import { Surface } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import { queryCommunities, createQuery, runQuery, sortByLastCreated } from '@amityco/ts-sdk';
 import React, { VFC, useState, useEffect, useLayoutEffect, useRef, useCallback } from 'react';
 
 import { Header, CommunityItem, EmptyComponent, FAB, AddCommunity, Loading } from 'components';
 
 import getErrorMessage from 'utils/getErrorMessage';
 
-import { CommunitySortBy, CommunityMembership, DrawerStackHeaderProps, LoadingState } from 'types';
+import { DrawerStackHeaderProps, LoadingState } from 'types';
+
+import styles from './styles';
 
 const QUERY_LIMIT = 5;
 
 const CommunitiesScreen: VFC = () => {
   const [isEditId, setIsEditId] = useState('');
   const [showAddCommunity, setShowAddCommunity] = useState(false);
-  const [membership] = useState<CommunityMembership>(CommunityMembership.MEMBER);
+  const [isDeleted] = React.useState<Amity.Community['isDeleted']>(false);
+  const [membership] = useState<'member' | 'notMember' | 'all'>('member');
   const [loading, setLoading] = useState<LoadingState>(LoadingState.NOT_LOADING);
-  const [isDeleted, setIsDeleted] = React.useState<Amity.Post['isDeleted']>(false);
-  const [sortBy, setSortBy] = React.useState<CommunitySortBy>(CommunitySortBy.LAST_CREATED);
+  const [sortBy] = React.useState<'firstCreated' | 'lastCreated' | 'displayName'>('lastCreated');
 
   const [communities, setCommunities] = useState<Record<string, Amity.Community>>({});
 
-  const [{ error, nextPage, loading: queryLoading }, setMetadata] = useState<
-    Amity.QueryMetadata & Amity.Pages
-  >({
+  const [{ error, nextPage }, setMetadata] = useState<Amity.QueryMetadata & Amity.Pages>({
     nextPage: null,
     prevPage: null,
   });
 
-  const flatlistRef = useRef<FlatList<Amity.Community>>(null);
+  const flatListRef = useRef<FlatList<Amity.Community>>(null);
 
   const navigation = useNavigation();
 
@@ -51,14 +50,23 @@ const CommunitiesScreen: VFC = () => {
         membership,
       };
 
-      runQuery(createQuery(queryCommunities, queryData), ({ data, ...metadata }) => {
-        if (reset) setCommunities({});
+      runQuery(
+        createQuery(queryCommunities, queryData),
+        ({ data, loading: loading_, ...metadata }) => {
+          if (reset) setCommunities({});
 
-        setCommunities(prevCommunities => ({ ...prevCommunities, ...data }));
+          if (data) {
+            setCommunities(prevCommunities => ({ ...prevCommunities, ...data }));
 
-        // @ts-ignore
-        setMetadata(metadata);
-      });
+            // @ts-ignore
+            setMetadata(metadata);
+          }
+
+          if (!loading_) {
+            setLoading(LoadingState.NOT_LOADING);
+          }
+        },
+      );
     },
     [isDeleted, membership, sortBy],
   );
@@ -72,16 +80,10 @@ const CommunitiesScreen: VFC = () => {
     onQueryCommunities({ reset: true });
   }, [onQueryCommunities]);
 
-  useEffect(() => {
-    if (!queryLoading) {
-      setLoading(LoadingState.NOT_LOADING);
-    }
-  }, [queryLoading]);
-
   React.useEffect(() => {
-    const unsubscribe = navigation?.dangerouslyGetParent()?.addListener('tabPress', e => {
+    const unsubscribe = navigation?.dangerouslyGetParent()?.addListener('tabPress', () => {
       onRefresh();
-      flatlistRef?.current?.scrollToOffset({ animated: true, offset: 0 });
+      flatListRef?.current?.scrollToOffset({ animated: true, offset: 0 });
     });
 
     return unsubscribe;
@@ -94,38 +96,50 @@ const CommunitiesScreen: VFC = () => {
     }
   };
 
-  const data = Object.values(communities);
+  const onCloseAddCommunity = useCallback(() => {
+    setIsEditId('');
+    setShowAddCommunity(false);
+  }, []);
+
+  const onEditCommunity = useCallback(id => {
+    setIsEditId(id);
+  }, []);
+
+  const onPressCommunityItem = useCallback(
+    community => {
+      navigation.navigate('Community', { community });
+    },
+    [navigation],
+  );
+
   const errorText = getErrorMessage(error);
+  const data = Object.values(communities)
+    .filter(post => (!isDeleted ? !post.isDeleted : true))
+    .sort(sortByLastCreated);
   const visibleAddCommunity = showAddCommunity || isEditId !== '';
 
   return (
     <Surface style={styles.container}>
       <FlatList
         data={data}
-        ref={flatlistRef}
+        ref={flatListRef}
         onRefresh={onRefresh}
         onEndReached={handleLoadMore}
         showsVerticalScrollIndicator={false}
         keyExtractor={item => item.communityId}
-        refreshing={loading === LoadingState.IS_REFRESHING || !!queryLoading}
-        ListFooterComponent={
-          loading === LoadingState.IS_LOADING_MORE && !!queryLoading ? <Loading /> : undefined
-        }
+        refreshing={loading === LoadingState.IS_REFRESHING}
+        ListFooterComponent={loading === LoadingState.IS_LOADING_MORE ? <Loading /> : undefined}
         renderItem={({ item }) => (
           <Surface style={styles.communityItem}>
             <CommunityItem
               community={item}
-              onEditCommunity={id => {
-                setIsEditId(id);
-              }}
-              onPress={() => {
-                navigation.navigate('Community', { community: item });
-              }}
+              onEditCommunity={onEditCommunity}
+              onPress={() => onPressCommunityItem(item)}
             />
           </Surface>
         )}
         ListEmptyComponent={
-          loading === LoadingState.NOT_LOADING && !queryLoading ? (
+          loading === LoadingState.NOT_LOADING ? (
             <EmptyComponent errorText={error ? errorText : undefined} />
           ) : null
         }
@@ -133,13 +147,9 @@ const CommunitiesScreen: VFC = () => {
 
       {visibleAddCommunity && (
         <AddCommunity
-          onClose={() => {
-            setIsEditId('');
-            setShowAddCommunity(false);
-          }}
           isEditId={isEditId}
           onAddCommunity={onRefresh}
-          visible
+          onClose={onCloseAddCommunity}
         />
       )}
 
@@ -152,16 +162,5 @@ const CommunitiesScreen: VFC = () => {
     </Surface>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  communityItem: {
-    flex: 1,
-    margin: 12,
-    borderRadius: 5,
-  },
-});
 
 export default CommunitiesScreen;
---
+++ src/screens/Communities/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  communityItem: {
+    flex: 1,
+    margin: 12,
+    borderRadius: 5,
+  },
+});
+
+export default styles;
