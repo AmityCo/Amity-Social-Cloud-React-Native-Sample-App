ebb233969fab979811c96680b736af52f1fbc245
Reza Babaei <ramyariran@gmail.com>
2022-03-01 00:15:24 +0700

    feat: feed

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -7,16 +7,16 @@ import {
   queryPosts,
   createQuery,
   runQuery,
-  // observePosts,
-  // queryGlobalFeed,
+  queryGlobalFeed,
   sortByLastCreated,
 } from '@amityco/ts-sdk';
 
-import { LoadingState, PostFeedType, FeedTargetType } from 'types';
+import getErrorMessage from 'utils/getErrorMessage';
+import { PostFeedType, FeedTargetType } from 'types';
 
 import Loading from '../Loading';
 import PostItem from '../PostItem';
-// import EmptyComponent from '../EmptyComponent';
+import EmptyComponent from '../EmptyComponent';
 
 import styles from './styles';
 
@@ -36,20 +36,22 @@ const FeedComponent: VFC<FeedComponentType> = ({
   targetId,
   targetType,
   isDeleted = false,
-  // feedTargetType = FeedTargetType.Normal,
+  feedTargetType = FeedTargetType.Normal,
   postFeedType = PostFeedType.PUBLISHED,
 }) => {
-  const [loading, setLoading] = useState<LoadingState>(LoadingState.NOT_LOADING);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   const [posts, setPosts] = useState<Amity.Post[]>([]);
 
-  const [{ nextPage }, setMetadata] = useState<Amity.SnapshotOptions & Amity.Pages>({
-    error: null,
-    nextPage: null,
-    prevPage: null,
-    loading: false,
-    origin: 'local',
-  });
+  const [{ nextPage, error, loading }, setMetadata] = useState<Amity.SnapshotOptions & Amity.Pages>(
+    {
+      error: null,
+      nextPage: null,
+      prevPage: null,
+      loading: false,
+      origin: 'local',
+    },
+  );
 
   const flatListRef = useRef<FlatList<Amity.Post>>(null);
 
@@ -57,37 +59,35 @@ const FeedComponent: VFC<FeedComponentType> = ({
 
   const onQueryPost = useCallback(
     async ({ reset = false, page = { limit: QUERY_LIMIT } }) => {
-      // let query = createQuery(queryGlobalFeed, { page });
+      let query = createQuery(queryGlobalFeed, { page });
 
-      // if (feedTargetType === FeedTargetType.Normal) {
-      const queryData = {
-        page,
-        isDeleted,
-        targetType,
-        targetId,
-        feedType: postFeedType,
-      };
+      if (feedTargetType === FeedTargetType.Normal) {
+        const queryData = {
+          page,
+          isDeleted,
+          targetType,
+          targetId,
+          feedType: postFeedType,
+        };
 
-      const query = createQuery(queryPosts, queryData);
+        // @ts-ignore
+        query = createQuery(queryPosts, queryData);
+      }
 
       runQuery(query, ({ data, ...metadata }) => {
         if (data) {
           setPosts(prevPosts => (reset ? data : [...prevPosts, ...data]));
         }
 
-        // console.log(page, { data, metadata });
-
         setMetadata(metadata);
 
         if (metadata?.loading === false) {
-          setLoading(LoadingState.NOT_LOADING);
+          setIsRefreshing(false);
         }
       });
     },
-    [isDeleted, postFeedType, targetId, targetType],
+    [feedTargetType, isDeleted, postFeedType, targetId, targetType],
   );
-
-  // console.log({ posts, nextPage, prevPage });
 
   // useEffect(
   //   () =>
@@ -109,7 +109,7 @@ const FeedComponent: VFC<FeedComponentType> = ({
   // );
 
   const onRefresh = useCallback(() => {
-    setLoading(LoadingState.IS_REFRESHING);
+    setIsRefreshing(true);
     onQueryPost({ reset: true });
   }, [onQueryPost]);
 
@@ -128,12 +128,11 @@ const FeedComponent: VFC<FeedComponentType> = ({
 
   const handleLoadMore = () => {
     if (nextPage) {
-      setLoading(LoadingState.IS_LOADING_MORE);
       onQueryPost({ page: nextPage });
     }
   };
 
-  // const errorText = getErrorMessage(error);
+  const errorText = getErrorMessage(error);
   const data = posts.filter(post => (!isDeleted ? !post.isDeleted : true)).sort(sortByLastCreated);
 
   return (
@@ -143,13 +142,11 @@ const FeedComponent: VFC<FeedComponentType> = ({
       ListHeaderComponent={header}
       keyExtractor={post => post.postId}
       showsVerticalScrollIndicator={false}
-      refreshing={loading === LoadingState.IS_REFRESHING}
-      ListFooterComponent={loading === LoadingState.IS_LOADING_MORE ? <Loading /> : undefined}
-      // ListEmptyComponent={
-      //   loading === LoadingState.NOT_LOADING ? (
-      //     <EmptyComponent errorText={error ? errorText : undefined} />
-      //   ) : null
-      // }
+      refreshing={isRefreshing}
+      ListFooterComponent={loading ? <Loading /> : undefined}
+      ListEmptyComponent={
+        loading ? <EmptyComponent errorText={error ? errorText : undefined} /> : null
+      }
       renderItem={({ item }) => (
         <Surface style={styles.postItem}>
           <PostItem
